474ccc6e7ffd6d53f61cb53c152b582feca2ea6e
Guilherme Gomes <guigomes@MacBook-Pro-de-Guilherme.local>
2021-07-21 19:59:43 +0200

    Add basic routes using react router

--- src/Palette.js
+++ src/Palette.js
@@ -17,10 +17,10 @@ class Palette extends Component {
         this.setState({ level })
     }
     render() {
-        const { colors } = this.props.palette;
+        const { colors, paletteName, emoji } = this.props.palette;
         const { level, format } = this.state;
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color[format]} name={color.name} />
+            <ColorBox background={color[format]} name={color.name} key={color.id} />
         ));
         return (
             <div className="Palette">
@@ -28,6 +28,10 @@ class Palette extends Component {
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
+                <fotter className="Palette-footer">
+                    {paletteName}
+                    <span className="emoji">{emoji}</span>
+                </fotter>
             </div>
         )
     }
