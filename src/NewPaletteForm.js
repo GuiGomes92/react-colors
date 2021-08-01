f0c4a73773239138292aec503b41bedc27e6e459
Guilherme Gomes <guigomes@MacBook-Pro-de-Guilherme.local>
2021-08-01 21:09:57 +0200

    Added delete dialog

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -125,6 +125,7 @@ class NewPaletteForm extends Component {
                         removeColor={this.removeColor}
                         axis='xy'
                         onSortEnd={this.onSortEnd}
+                        distance={20}
                     />
                 </main>
             </div >
